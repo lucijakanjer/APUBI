836dfa952e66db90e064beeed30d88465167c078
Lucija Kanjer <lucija.kanjer@gmail.com>
2024-09-25 12:03:13 +0200

    rmd 3

--- skripte/3_Rad_s _objektima.R
+++ skripte/3_Rad_s _objektima.R
@@ -1,23 +1,79 @@
-### Rad s objektima i podacima te definiranje bioloških varijabli
+library(tidyverse)
 
-# Manipulacija objektima u R-u
+# Učitavanje paketa s podacima o Palmer pingvinima
+library(palmerpenguins)
 
-# Filtriranje podataka
-Adelie_penguins <- subset(penguins, species == "Adelie")
+# Učitavanje podataka u R okruženje
+data("penguins")
 
-# Sortiranje podataka
-sorted_penguins <- penguins[order(penguins$bill_length_mm), ]
+View(penguins)
 
-# Grupiranje podataka prema vrsti
-library(dplyr)
-grouped_data <- penguins %>% 
-  group_by(species) %>% 
-  summarise(mean_bill_length = mean(bill_length_mm, na.rm = TRUE))
+# Odabir (selektiranje) varijabli od interesa
+penguins %>%
+  select(species, island, body_mass_g)
 
-# Definiranje bioloških varijabli
-# Kreiranje nove varijable koja označava veličinu kljuna
-penguins$bill_size <- ifelse(penguins$bill_length_mm > 40, "large", "small")
+# Spremanje novog data frame-a kao novi objekt "adelie"
+adelie <- penguins %>%
+  select(species, island, body_mass_g)
 
-# Zadaci
-# Filtrirajte podatke za vrstu "Chinstrap" i izračunajte prosječnu duljinu kljuna.
-# Kreirajte novu varijablu koja kategorizira pingvine prema težini.
+# Provjera tipa i strukture objekta
+str(adelie)
+
+# filtriramo varijablu "species" da sadržava samo vrijednosti "Adelie"
+adelie <- penguins %>%
+  select(species, island, body_mass_g) %>%
+  filter(species == "Adelie")
+
+# Kreiramo novu varijablu "body_mass_kg" tako da staru varijablu "body_mass_g" podijelimo s 1000
+adelie <- penguins %>%
+  select(species, island, body_mass_g) %>%
+  filter(species == "Adelie") %>%
+  mutate(body_mass_kg = body_mass_g/1000)
+
+# Ako želimo da se rezultati prikazuju po otocima, moramo koristiti naredbu group_by()
+adelie <- penguins %>%
+  select(species, island, body_mass_g) %>%
+  filter(species == "Adelie") %>%
+  mutate(body_mass_kg = body_mass_g/1000) %>%
+  group_by(island)
+
+# Kako bi se prikazao prosjek moramo koristiti naredbu summarise()
+adelie_result <- penguins %>%
+  select(species, island, body_mass_g) %>%
+  filter(species == "Adelie") %>%
+  mutate(body_mass_kg = body_mass_g/1000) %>%
+  group_by(island) %>%
+  summarise(average_mass = mean(body_mass_kg))
+
+print(adelie_result)
+View(adelie_result)
+
+# Uklanjanje nedostajućih vrijednosti
+adelie_result <- penguins %>%
+  select(species, island, body_mass_g) %>%
+  filter(species == "Adelie") %>%
+  mutate(body_mass_kg = body_mass_g/1000) %>%
+  na.omit() %>%
+  group_by(island) %>%
+  summarise(average_mass = mean(body_mass_kg))
+
+print(adelie_result)
+
+
+# zadatak
+
+gentoo <- penguins %>%
+  select(species, sex, body_mass_g) %>%
+  filter(species == "Gentoo") %>%
+  mutate(body_mass_kg = body_mass_g/1000)
+  
+
+gentoo_result <- penguins %>%
+  select(species, sex, body_mass_g) %>%
+  filter(species == "Gentoo") %>%
+  mutate(body_mass_kg = body_mass_g/1000) %>%
+  na.omit() %>%
+  group_by(sex) %>%
+  summarise(average_mass = mean(body_mass_kg))
+
+print(gentoo_result)
